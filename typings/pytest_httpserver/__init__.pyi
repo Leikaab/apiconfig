a958892310613eb80a03b945a677b2e5a682a3cb
Leikaab <49791701+Leikaab@users.noreply.github.com>
2025-06-09 10:16:06 +0200

    Fix merge conflict in pytest_httpserver stub

--- typings/pytest_httpserver/__init__.pyi
+++ typings/pytest_httpserver/__init__.pyi
@@ -1,9 +1,8 @@
-from typing import Any, List, Mapping, Tuple
+from typing import Any, List, Mapping, Pattern, Tuple
 
 from werkzeug.wrappers import Request, Response
 
 class RequestHandler:
-    def respond_with_response(self, response: Response, **kwargs: Any) -> None: ...
     def respond_with_json(
         self,
         response_json: Any,
@@ -11,8 +10,16 @@ class RequestHandler:
         headers: Mapping[str, str] | None = None,
         content_type: str = "application/json",
     ) -> None: ...
+    def respond_with_response(self, response: Response, **kwargs: Any) -> None: ...
 
 class HTTPServer:
     log: List[Tuple[Request, Response]]
-    def expect_request(self, uri: str, method: str = "GET", *, ordered: bool = ..., **kwargs: Any) -> RequestHandler: ...
+    def expect_request(
+        self,
+        uri: str | Pattern[str],
+        method: str = "GET",
+        *,
+        ordered: bool = ...,
+        **kwargs: Any,
+    ) -> RequestHandler: ...
     def url_for(self, suffix: str) -> str: ...

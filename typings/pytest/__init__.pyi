16eb75930bfb6dc2da6a270ae502c7e7d0c3a27e
leikaab <nordavindltd@gmail.com>
2025-06-11 18:37:59 +0000

    fix: autofix with isort

--- typings/pytest/__init__.pyi
+++ typings/pytest/__init__.pyi
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 from _pytest.fixtures import fixture
 from _pytest.mark import MARK_GEN as mark
-from _pytest.python_api import raises
 from _pytest.monkeypatch import MonkeyPatch
 from _pytest.outcomes import fail, importorskip, skip
+from _pytest.python_api import raises
 
 parametrize = mark.parametrize
 

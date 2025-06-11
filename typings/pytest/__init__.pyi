381e1cef303de5f3a6e37dd92bd3ab447a76aa0a
Leikaab <49791701+Leikaab@users.noreply.github.com>
2025-06-11 17:16:39 +0200

    feat: add basic pytest stubs

---
+++ typings/pytest/__init__.pyi
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from _pytest.fixtures import fixture
+from _pytest.mark import MARK_GEN as mark
+from _pytest.python_api import raises
+from _pytest.monkeypatch import MonkeyPatch
+from _pytest.outcomes import fail, importorskip, skip
+
+parametrize = mark.parametrize
+
+__all__ = [
+    "fixture",
+    "mark",
+    "parametrize",
+    "raises",
+    "MonkeyPatch",
+    "fail",
+    "skip",
+    "importorskip",
+]
